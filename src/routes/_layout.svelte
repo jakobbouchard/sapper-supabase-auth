62be35ad7888544af5e85e810fa77f5d5ccdd81f
Jakob Bouchard <24767889+jakobbouchard@users.noreply.github.com>
2021-02-07 17:15:12 -0500

    Keeping the user logged in

--- src/routes/_layout.svelte
+++ src/routes/_layout.svelte
@@ -1,7 +1,37 @@
 <script>
+	import { onMount } from 'svelte';
+	import { stores } from '@sapper/app'
+	const { session } = stores()
 	import Nav from '../components/Nav.svelte';
 
 	export let segment;
+
+	onMount(async () => {
+		if (!$session.userToken) {
+			const refresh = await fetch(`/api/refresh`, {
+				method: 'POST',
+				credentials: 'same-origin'
+			});
+			const { supaToken } = await refresh.json();
+			if (supaToken) {
+				return session.set({ userToken: supaToken });
+			}
+			return;
+		}
+
+		// refreshes token every 55 minutes to also sync with server-side.
+		setInterval(async () => {
+			const refresh = await fetch(`/api/refresh`, {
+				method: 'POST',
+				credentials: 'same-origin'
+			});
+			const { supaToken } = await refresh.json();
+			if (supaToken) {
+				return session.set({ userToken: supaToken });
+			}
+			console.log('No user, timeout will be killed eventually')
+		}, 1000 * 60 * 55);
+	});
 </script>
 
 <style>
